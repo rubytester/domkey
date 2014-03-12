689b14b1013491dcd5b2a69203b1abfda8dba6c1
rubytester <github@rubytester.com>
2014-03-12 12:07:10 -0500

    first iteration on composed pageobject
    send hash with keys adn values as lambda

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -1,50 +1,42 @@
 require 'spec_helper'
-
-# Page is a context for pageobject collection
-
-#page owns access to browser driver, provides browser as container to page objects
-#page is responsible for constructing pageobjects available in that page
-class ExamplePage
-  # client class needs to inluce this to play a role of a page
-  include Domkey::Page
-
-  # single element with default container browser
-  dom(:street) { text_field(id: 'street1') }
-
-  # single element take pageobject as its definition
-  #dom(:street_again) { street }
-
-  # single element where container is another pageobject
-  dom(:city2) do
-    PageObject.new(
-        lambda { text_field(class: 'city') },
-        lambda { div(id: 'container') }
-    )
+module DomkeyExample
+  class DomIsWatirproc
+    include Domkey::Page
+    #pageobject is watirproc
+    dom(:street) { text_field(id: 'street1') }
+    dom(:city) { text_field(id: 'city1') }
   end
 
-  # TODO: pageobject matcher. Return page objects collection based on criteria runtime
-  #dom(:city) { |arg| text_field(id:, arg) }
+  class DomIsHashOfDom # or doom
+    include Domkey::Page
 
-  dom(:city) { text_field(id: 'city') }
+    # literal hash with lambda values
+    dom(:address) do
+      {
+          street: -> { text_field(id: 'street2') },
+          city:   -> { text_field(id: 'city2') }
+      }
+    end
 
-  dom(:address_composed) do
-    {city: city, street: street}
+    ## alternative api design
+
+    #dom(:address) do
+    #  Proc.new { Hash.new(:street => dom(:street) { text_field(id: 'street2') }) }
+    #  {:street => (dom(:street) { text_field(id: 'street2') })} #literal hash
+    #end
+
+    #composed
+    #dom(:address) do
+    #  {street: street,
+    #   city:   city}
+    #end
+
+    ## pageobject is composed
+    #domkey(:address) do
+    #  dom(:street) { text_field(id: 'street1') }
+    #  dom(:city) { text_field(id: 'city') }
+    #end
   end
-
-  dom(:address) do
-    {city:   lambda { text_field(id: 'street1') },
-     street: lambda { text_field(id: 'street1') }}
-  end
-
-  # composed element
-  #dom(:address_bla) do
-  #  dom(street:) { text_field(id: 'street1') }, dom(city:) { text_field(id:, 'city') }
-  #end
-
-  # pageobject from other page objects
-  #dom(:address2) { PageObject.new(street: street, city: city) }
-  #    dom(:feature) { CheckboxTextField.new(switch: '', blurb: '') }
-
 end
 
 describe Domkey::Page do
@@ -53,34 +45,45 @@ describe Domkey::Page do
     Domkey.browser.goto("file://" + __dir__ + "/html/test.html")
   end
 
-  before :all do
-    @container = lambda { Domkey.browser }
-  end
-
-  it 'watirproc' do
-
-    #watirproc = lambda { text_field(id: 'street1') }
-    #street    = Domkey::PageObject.new watirproc, @container
-    #street.set 'Lamar'
-    #street.value.should eql 'Lamar'
-    #street.element.should be_kind_of(Watir::TextField) #one default element
-
-    view = ExamplePage.new
-    view.should respond_to(:street) #.value.should eql 'Lamar'
+  it 'domw is watirproc' do
+    view = DomkeyExample::DomIsWatirproc.new
+    view.should respond_to(:street)
     view.street.should be_kind_of(Domkey::Page::PageObject)
 
-    view.street.set 'BlaBlaBla'
-
-    view.city2.set 'asdfasdfasdfadsf'
+    # talk to browser
+    view.street.set 'hello dom'
+    view.street.value.should eql 'hello dom'
   end
 
-  #it 'pageobject' do
-  #  watir_object             = lambda { text_field(id: 'street1') }
-  #  street_from_watir_object = Domkey::PageObject.new watir_object, @container
-  #  street                   = Domkey::PageObject.new street_from_watir_object, @container
-  #
-  #  street.set 'zooom' #sending string here so no hash like in composed object
-  #  street.value.should eql 'zooom'
-  #  street.element.should be_kind_of(Watir::TextField)
-  #end
+  it 'dom is hash of dom' do
+    view = DomkeyExample::DomIsHashOfDom.new
+
+    view.should respond_to(:address)
+
+    view.address.watirproc.should respond_to(:each_pair)
+    view.address.watirproc.should_not be_empty
+
+    view.address.watirproc.each_pair do |k, v|
+      k.should be_kind_of(Symbol)
+      v.should be_kind_of(Domkey::Page::PageObject) #should respond to set and value
+    end
+
+    view.address.element.should respond_to(:each_pair)
+    view.address.element.each_pair do |k, v|
+      v.should be_kind_of(Watir::TextField) #resolve suitecase
+    end
+
+
+    view.address.element.keys.should eql [:street, :city]
+
+    # talk to browser
+    value = {street: 'Quantanemera', city: 'Austin'}
+    view.address.set value
+    v = view.address.value
+    v.should eql value
+
+    #TODO : set partial address
+    #value = {street: 'Lamar'}
+
+  end
 end
