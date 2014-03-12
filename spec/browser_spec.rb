2d23ff05e6e151a3644ae2153654a8b9c63bca69
rubytester <github@rubytester.com>
2014-03-12 14:13:36 -0500

    reenable browser test for caching

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -1,29 +1,25 @@
-#require 'spec_helper'
-#
-#describe Domkey do
-#
-#  context 'when no browsers present in the system' do
-#
-#    before do
-#      Domkey.browser = nil
-#      @browser       = double('browser')
-#      @browser.stub(:exist?).and_return(true)
-#      Watir::Browser.should_receive(:new).once.and_return(@browser)
-#    end
-#
-#    after do
-#      Domkey.browser.close
-#    end
-#
-#    it '.browser gives us default browser' do
-#      b = Domkey.browser
-#      b.should eq @browser
-#    end
-#
-#    it 'once we start browser we reuse it' do
-#      b  = Domkey.browser
-#      b2 = Domkey.browser
-#      b.should eql b2
-#    end
-#  end
-#end
+require 'spec_helper'
+
+describe Domkey do
+
+  context 'when no browsers present in the system' do
+
+    before do
+      Domkey.browser = nil
+      @browser       = double('browser')
+      @browser.stub(:exist?).and_return(true)
+      Watir::Browser.should_receive(:new).once.and_return(@browser)
+    end
+
+    it '.browser gives us default browser' do
+      b = Domkey.browser
+      b.should eq @browser
+    end
+
+    it 'once we start browser we reuse it' do
+      b  = Domkey.browser
+      b2 = Domkey.browser
+      b.should eql b2
+    end
+  end
+end
