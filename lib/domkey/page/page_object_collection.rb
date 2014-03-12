c3e3b649d8fd6247a22ce8d484cfad8d3a8cfb7d
rubytester <github@rubytester.com>
2014-03-11 22:20:37 -0500

    wip PageObjectCollection

--- lib/domkey/page/page_object_collection.rb
+++ lib/domkey/page/page_object_collection.rb
@@ -1,15 +1,61 @@
+require 'domkey/page/page_object'
 module Domkey
 
   module Page
 
-    class PageObjectCollection < PageObject
+    class PageObjectCollection
       include Enumerable
 
-      # each PageObject from a collection defined by watirproc
-      def each
-        instantiator.each do |e|
-          yield PageObject.new(lambda { e }, @container)
+      attr_accessor :watirproc, :container
+
+      # watirproc is either
+      #   a proc that defines watir collection i.e lambda {class: /^street/}
+      #   or a hash where value in each key is a proc
+      # basically some widgetry to interaction with watir collection
+      def initialize watirproc, container=lambda { Domkey.browser }
+        @container = container
+        @watirproc = initialize_this watirproc
+      end
+
+      # --
+      # recursive
+      def initialize_this watirproc
+        if watirproc.respond_to?(:each_pair)
+          Hash[watirproc.map { |key, watirproc| [key, PageObjectCollection.new(watirproc, container)] }]
+        else
+          if watirproc.respond_to?(:call)
+            watirproc
+          elsif watirproc.respond_to?(:watirproc)
+            watirproc.watirproc
+          else
+            fail Domkey::PageObjectError, "Unable to construct PageObjectCollection using definition: #{watirproc}"
+          end
         end
+      end
+
+      def element(key=false)
+        return watirproc.fetch(key).instantiator if key
+        return instantiator unless watirproc.respond_to?(:each_pair)
+        Hash[watirproc.map { |key, watirproc| [key, watirproc.instantiator] }]
+      end
+
+      def each(&blk)
+        if watirproc.respond_to?(:each_pair)
+          watirproc.map { |k, v| [k, PageObjectCollection.new(lambda { v }, @container)] }.each { |k, v| yield Hash[k,v] }
+        else
+          instantiator.each { |e| yield PageObject.new(lambda { e }, @container) }
+        end
+      end
+
+      # runtime dom element in a specified container or collection of dom elements
+      def instantiator
+        container_at_runtime.instance_exec(&watirproc)
+      end
+
+      # container at runtime could be a proc or an actual page object
+      # proc we call. pageobject we send dom message to get gack runtime container
+      def container_at_runtime
+        container.respond_to?(:call) ? container.call : container.instantiator
       end
 
       def [] idx
@@ -17,20 +63,6 @@ module Domkey
       end
 
       alias_method :size, :count
-
-      # ---------------- this is only for pageobject
-
-      def set
-        fail
-      end
-
-      def value
-        fail
-      end
-
-      def element
-        fail
-      end
 
     end
   end
--- spec/page_object_collection_spec.rb
+++ spec/page_object_collection_spec.rb
@@ -11,55 +11,70 @@ describe Domkey::Page::PageObjectCollection do
     @container = lambda { Domkey.browser }
   end
 
-  context 'examples using browser' do
+  it 'single watirproc defining collection' do
 
-    before :all do
+    # watirproc is now defininig a collection and not a single element
+    # we have 3 text_fields with class that begins with street
+    # if we define a single element we go boom
+    watirproc = lambda { text_fields(class: /^street/) }
+    cbs       = Domkey::Page::PageObjectCollection.new watirproc, @container
 
-      # watirproc is now defininig a collection and not a single element
-      # we have 3 text_fields with class that begins with street
-      # if we define a single element we go boom
-      watirproc = lambda { text_fields(class: /^street/) }
-      @cbs      = Domkey::Page::PageObjectCollection.new watirproc, @container
+    # count or size
+    cbs.count.should == 3
+
+    #each returns PageObject
+    cbs.each do |e|
+      e.should be_kind_of(Domkey::Page::PageObject)
     end
 
-    it 'count' do
-      # count or size
-      @cbs.count.should == 3
-    end
+    # PageOobject by index
+    cbs[0].should be_kind_of(Domkey::Page::PageObject)
+    cbs[1].should be_kind_of(Domkey::Page::PageObject)
 
-    it 'query each is PageObject' do
-      #each returns PageObject
-      @cbs.each do |e|
-        e.should be_kind_of(Domkey::Page::PageObject)
-      end
-    end
+    # find_all of some condition
+    cbs.find_all { |e| e.value == 'hello pageobject' }.should be_empty
 
-    it 'query by [index]' do
-      # PageOobject by index
-      @cbs[0].should be_kind_of(Domkey::Page::PageObject)
-      @cbs[1].should be_kind_of(Domkey::Page::PageObject)
-    end
+    cbs.first.set 'hello pageobject'
 
-    it 'find_all and changing condition' do
-
-      # find_all of some condition
-      @cbs.find_all { |e| e.value == 'hello pageobject' }.should be_empty
-
-      @cbs.first.set 'hello pageobject'
-
-      # find_all returns the one we just set
-      @cbs.find_all { |e| e.value == 'hello pageobject' }.count.should eql(1)
+    # find_all returns the one we just set
+    cbs.find_all { |e| e.value == 'hello pageobject' }.count.should eql(1)
 
 
-      # set value to later return
-      @cbs[1].set 'bye bye'
+    # set value to later return
+    cbs[1].set 'bye bye'
 
-      # map iterates and harvests value
-      @cbs.map { |e| e.value }.should eql ["hello pageobject", "bye bye", ""]
-
-    end
+    # map iterates and harvests value
+    cbs.map { |e| e.value }.should eql ["hello pageobject", "bye bye", ""]
   end
 
+  it 'hash watirprocs' do
+    #would we do that? give me all text_fields :street and all text_fields :city ? in one collection?
+    #secondary usage
+    # given I have city 4 and street 3 textfields
+    Domkey.browser.text_fields(class: /^street/).count.should == 3
+    Domkey.browser.text_fields(class: /^city/).count.should == 4
+
+    # when I define my keyed collection
+    watirproc = {street: lambda { text_fields(class: /^street/) },
+                 city:   lambda { text_fields(class: /^city/) }}
+
+    cbs = Domkey::Page::PageObjectCollection.new watirproc, @container
+
+    # to_a array array of hashes. Each hash key and value is pageobjectcollection
+    cbs.to_a.should have(2).items
+
+    #street: pageobjectcollection
+    street_hash = cbs.to_a[0]
+    street_hash.should be_kind_of(Hash)
+    street_collection = street_hash[:street]
+    street_collection.should be_kind_of(Domkey::Page::PageObjectCollection)
+    streets = street_collection.to_a
+    streets.should have(3).items
+    streets.each { |e| e.should be_kind_of(Domkey::Page::PageObject) }
+
+    # each
+    cbs.each do |hash|
+      hash.each_pair { |k, v| v.should be_kind_of(Domkey::Page::PageObjectCollection) }
+    end
+  end
 end
-
-
