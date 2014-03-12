3b6ff6f437c27e7dde9a8347635bc376c3f27d90
rubytester <github@rubytester.com>
2014-03-12 15:40:49 -0500

    back to browser. isolated singleton in test

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -1,25 +1,23 @@
-#require 'spec_helper'
-#
-#describe Domkey do
-#
-#  context 'when no browsers present in the system' do
-#
-#    before do
-#      Domkey.browser = nil
-#      @browser       = double('browser')
-#      @browser.stub(:exist?).and_return(true)
-#      Watir::Browser.should_receive(:new).once.and_return(@browser)
-#    end
-#
-#    it '.browser gives us default browser' do
-#      b = Domkey.browser
-#      b.should eq @browser
-#    end
-#
-#    it 'once we start browser we reuse it' do
-#      b  = Domkey.browser
-#      b2 = Domkey.browser
-#      b.should eql b2
-#    end
-#  end
-#end
+require 'spec_helper'
+
+describe Domkey::BrowserSession do
+
+  context 'when no browsers present in the system' do
+
+    it 'browser gives us default browser' do
+      #ugly setup with singleton
+      Domkey::BrowserSession.instance.browser.close
+      Domkey::browser=nil
+
+      fakebrowser = double('browser')
+      fakebrowser.stub(:exist?).and_return(true)
+      Watir::Browser.should_receive(:new).once.and_return(fakebrowser)
+      b = Domkey.browser
+      b.should eq fakebrowser
+      b2 = Domkey.browser
+      b.should eql b2
+      Domkey::BrowserSession.instance.browser=nil
+    end
+
+  end
+end
