2064ec8f4b254d35e82d5f09a3f3f584beabc5ec
rubytester <github@rubytester.com>
2014-03-12 19:45:37 -0500

    add simplecov dev dependency for coverage goodness

--- domkey.gemspec
+++ domkey.gemspec
@@ -25,5 +25,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.5'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 2.14'
+  spec.add_development_dependency 'simplecov'
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rspec'
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+end
 require 'domkey'
+SimpleCov.command_name "test:units"
 
 RSpec.configure do |config|
   config.after :all do
