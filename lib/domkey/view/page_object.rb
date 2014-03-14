014f4d44a709a294cbfbf30d9417ec318f2c07cc
rubytester <github@rubytester.com>
2014-03-14 18:04:27 -0500

    document yarddoc lika a boss. like abbas

--- lib/domkey/view/page_object.rb
+++ lib/domkey/view/page_object.rb
@@ -2,6 +2,50 @@ module Domkey
 
   module View
 
+    # PageObject represents an semantically essential area in a View
+    # It is an object that responds to set and value as the main way of sending data to it.
+    # it is composed of one or more watir elements.
+    # PageObject encapuslates the widgetry of DOM elements to provide semantic interfact to the user of the widgetry
+    #
+    # Compose PageObject with package and container
+    #
+    # What is a container? it's a proc, a callable object that plays a role of a container for package widgetry
+    # container can be one of:
+    # - browser (default)
+    # - a pageobject
+    #
+    # What is package? it's a proc of DOM elements widgetry that can be found inside the container
+    # package can be one of the following:
+    #   - definition of single watir element i.e. `-> { text_field(:id, 'foo')}`
+    #   - a pageobject i.e. previously instantiated definition
+    #   - hash where key defines subelement and value a definition or pageobject
+    #
+    # Usage:
+    # Clients would not usually instantate this class.
+    # A client class which acts as a View would use a :dom factory method to create PageObjects
+    # Example:
+    #
+    #        class MyView
+    #          include Domkey::View
+    #
+    #          dom(:headline) { text_field(id:, 'some_desc_text') }
+    #
+    #          def property
+    #            PropertyPanel.new browser.div(id: 'container')
+    #          end
+    #        end
+    #
+    #        class PropertyPanel
+    #          include Domkey::View
+    #          dom(:headline) { text_field(class: 'headline_for_house') }
+    #        end
+    #
+    #        view = MyView.new
+    #        view.headline.set 'HomeAway Rules!'
+    #        view.value #=> returns 'HomeAway Rules!'
+    #        view.property.headline.set 'Awesome Vactaion Home'
+    #        view.property.headline.value #=> returns 'Awesome Vaction Home'
+    #
     class PageObject
 
       # @api private
@@ -9,12 +53,22 @@ module Domkey
 
         attr_accessor :package, :container
 
+        # initialize PageObject or PageObjectCollection
+        # for PageObject expects WebdriverElement a single element definition i.e text_field, checkbox
+        # for PageObjectCollection expects WebdriverElement a collection definition i.e. text_fields, checkboxes
+        # @param package [Proc(WebdriverElement)]
+        # @param package [PageObject]
+        # @param package [Hash{Symbol => Proc(WebdriverElement)]
+        # @param package [Hash{Symbol => PageObject]
         def initialize package, container=lambda { Domkey.browser }
           @container = container
           @package   = initialize_this package
         end
 
         # access widgetry of watir elements composing this page object
+        # @param [Symbol] (false)
+        # @return [Hash{Symbol => WebdriverElement}]
+        # @return [WebdriverElement]
         def element(key=false)
           return instantiator unless package.respond_to?(:each_pair)
           return package.fetch(key).element if key
@@ -23,71 +77,35 @@ module Domkey
 
         private
 
-        # talk to the browser executor.
+        # talks to the browser
         # returns runtime element in a specified container
         # expects that element to respond to set and value
+        # @return [WebdriverElement]
         def instantiator
           container_instantiator.instance_exec(&package)
         end
 
-        # talk to the browser
+        # talks to the browser
         # returns runtime container element in a browser/driver
+        # @return [WebdriverElement]
         def container_instantiator
           container.respond_to?(:call) ? container.call : container.send(:instantiator)
         end
       end
 
-      # PageObject represents an semantically essential area in a View
-      # It is an object that responds to set and value as the main way of sending data to it.
-      # it is composed of one or more watir elements.
-      # PageObject encapuslates the widgetry of DOM elements to provide semantic interfact to the user of the widgetry
-      #
-      # Compose PageObject with package and container
-      #
-      # What is a container? it's a proc, a callable object that plays a role of a container for package widgetry
-      # container can be one of:
-      # - browser (default)
-      # - a pageobject
-      #
-      # What is package? it's a proc of DOM elements widgetry that can be found inside the container
-      # package can be one of the following:
-      #   - definition of single watir element i.e. `-> { text_field(:id, 'foo')}`
-      #   - a pageobject i.e. previously instantiated definition
-      #   - hash where key defines subelement and value a definition or pageobject
-      #
-      # Usage:
-      # Clients would not usually instantate this class.
-      # A client class which acts as a View would use a :dom factory method to create PageObjects
-      # Example:
-      #
-      #        class MyView
-      #          include Domkey::View
-      #
-      #          dom(:headline) { text_field(id:, 'some_desc_text') }
-      #
-      #          def property
-      #            PropertyPanel.new browser.div(id: 'container')
-      #          end
-      #        end
-      #
-      #        class PropertyPanel
-      #          include Domkey::View
-      #          dom(:headline) { text_field(class: 'headline_for_house') }
-      #        end
-      #
-      #        view = MyView.new
-      #        view.headline.set 'HomeAway Rules!'
-      #        view.value #=> returns 'HomeAway Rules!'
-      #        view.property.headline.set 'Awesome Vactaion Home'
-      #        view.property.headline.value #=> returns 'Awesome Vaction Home'
-      #
       include WidgetryPackage
 
+      # Each Semantic PageObject defines what value means for itself
+      # @param [SemanticValue] Delegated to WebdriverElement and we expect it to respond to set
+      # @parma [Hash{Symbol => SemanticValue}]
       def set value
         return instantiator.set(value) unless value.respond_to?(:each_pair)
         value.each_pair { |k, v| package.fetch(k).set(v) }
       end
 
+      # Each Semantic PageObject defines what value means for itself
+      # @return [SemanticValue] delegated to WebdriverElement and we expect it to respond to value message
+      # @return [Hash{Symbol => SemanticValue}]
       def value
         return instantiator.value unless package.respond_to?(:each_pair)
         Hash[package.map { |key, pageobject| [key, pageobject.value] }]
@@ -95,7 +113,8 @@ module Domkey
 
       private
 
-      # recursive
+      # @api private
+      # Recursive. Examines each packages and turns each Proc into PageObject
       def initialize_this package
         if package.respond_to?(:each_pair) #hash
           Hash[package.map { |key, package| [key, PageObject.new(package, container)] }]
--- lib/domkey/view/page_object_collection.rb
+++ lib/domkey/view/page_object_collection.rb
@@ -2,26 +2,26 @@ module Domkey
 
   module View
 
+    # PageObjectCollection see PageObject for detailes.
+    # Compose PageObjectCollection with package and container
+    #
+    # What is a container? see PageObject container
+    #
+    # What is package? see PageObject package except the following:
+    # package can be one of the following:
+    #   - definition of watir elements collection i.e. `-> { text_fields(:class, /^foo/)}`
+    #   - a pageobject i.e. previously instantiated definition watir elements collection
+    #   - hash where key defines subelement and value a definition or pageobject
+    # Usage:
+    # Clients would not usually instantate this class.
+    # A client class which acts as a View would use a :doms factory method to create PageObjectCollection
+    # TODO Example:
     class PageObjectCollection
 
       include PageObject::WidgetryPackage
       include Enumerable
 
-      # PageObjectCollection see PageObject for detailes.
-      # Compose PageObjectCollection with package and container
-      #
-      # What is a container? see PageObject container
-      #
-      # What is package? see PageObject package except the following:
-      # package can be one of the following:
-      #   - definition of watir elements collection i.e. `-> { text_fields(:class, /^foo/)}`
-      #   - a pageobject i.e. previously instantiated definition watir elements collection
-      #   - hash where key defines subelement and value a definition or pageobject
-      # Usage:
-      # Clients would not usually instantate this class.
-      # A client class which acts as a View would use a :doms factory method to create PageObjectCollection
-      # Example:
-
+      # @return [PageObject, Hash{Symbol => PageObjectCollection}]
       def each(&blk)
         if package.respond_to?(:each_pair)
           package.map { |k, v| [k, PageObjectCollection.new(lambda { v }, @container)] }.each { |k, v| yield Hash[k, v] }
@@ -30,6 +30,8 @@ module Domkey
         end
       end
 
+      # @param [Fixnum]
+      # @return [PageObject, Hash{Symbol => PageObjectCollection}]
       def [] idx
         to_a[idx]
       end
@@ -38,8 +40,8 @@ module Domkey
 
       private
 
-      # --
-      # recursive
+      # @api private
+      # Recursive. Examines each packages and turns each Proc into PageObject
       def initialize_this package
         if package.respond_to?(:each_pair) #hash
           Hash[package.map { |key, package| [key, PageObjectCollection.new(package, container)] }]
