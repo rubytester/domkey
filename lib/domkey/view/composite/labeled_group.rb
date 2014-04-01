68f5bb4efaee154c49fc556ba6f32253024fdef9
rubytester <github@rubytester.com>
2014-04-01 16:03:08 -0500

    refactor. how about we set individual radios or checkboxes from positions of labels in array.

--- lib/domkey/view/composite/labeled_group.rb
+++ lib/domkey/view/composite/labeled_group.rb
@@ -12,28 +12,33 @@ module Domkey
         # @param value [String] a label text to set
         # @param value [Array<String>] label texts to set
         def set value
-          container = @group.container.call
-          labels = @group.map { |e| container.label(for: e.element.id) }
-          values_to_set = []
-          if value.kind_of? Array
-            value.each do |v|
-              label = labels.find { |l| l.text == v }
-              values_to_set << @group.find { |e| e.element.id == label.for }.value unless label.nil?
-            end
-            @group.set values_to_set
-          else
-            label = labels.find { |l| l.text == value }
-            @group.set @group.find { |e| e.element.id == label.for }.value
+          @group.set false
+          labels  = build_labels.map { |e| e.element.text }
+          indices = [*value].map { |text| labels.index(text) }
+          indices.each do |i|
+            @group[i].element.set
           end
         end
 
         # @return Array of selected texts
         def value
           element = @group.element.find_all { |e| e.set? }
-          labels = element.map { |e| @group.container.call.label(for: e.id) }
+          labels  = element.map { |e| @group.container.call.label(for: e.id) }
           labels.map { |l| l.text }
         end
+
+        def build_labels
+          Labels.for @group
+        end
       end
+
+      class Labels
+        def self.for collection
+          ids = collection.map { |e| e.element.id }
+          ids.map { |id| Domkey::View::PageObject.new -> { label(for: id) }, collection.container }
+        end
+      end
+
     end
   end
 end
