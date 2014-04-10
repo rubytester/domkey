18266481ee9938cb45199c0dd2c256f8bec45b60
rubytester <github@rubytester.com>
2014-04-10 15:25:40 -0500

    refactor. cleanup

--- spec/binder_spec.rb
+++ spec/binder_spec.rb
@@ -39,67 +39,67 @@ describe Domkey::View::Binder do
     goto_html("test.html")
   end
 
-  it 'view within pageobject' do
-    payload = {city:   'Austin',
-               street: 'Lamar'}
+  context 'Binder payload set and value' do
 
-    binder = Domkey::View::Binder.new payload: payload, view: AddressView.new
-    binder.set
-    extracted = binder.value
-    extracted.should eql payload
+    it 'for single view' do
+      payload = {city: 'Austin', street: 'Lamar'}
+
+      binder = Domkey::View::Binder.new payload: payload, view: AddressView.new
+      binder.set
+
+      extracted = binder.value
+      extracted.should eql payload
+    end
+
+    it 'for view within a view' do
+      payload = {city:     'Austin', street: 'Lamar',
+                 shipping: {city: 'Georgetown', street: 'Austin'}
+      }
+
+      binder = Domkey::View::Binder.new payload: payload, view: AddressView.new
+      binder.set
+
+      extracted = binder.value
+      extracted.should eql payload
+
+    end
+
+    it 'for view view view' do
+
+      payload = {city:     'Austin', street: 'Lamar',
+                 shipping: {city:          'Georgetown', street: 'Austin',
+                            # this is view within a view within original view
+                            delivery_date: {month: 'delivery thing'}}
+      }
+
+      binder = Domkey::View::Binder.new payload: payload, view: AddressView.new
+      binder.set
+
+
+      extracted = binder.value
+      extracted.should eql payload
+
+    end
+
   end
 
-  it 'view within view' do
-    payload = {city:     'Austin',
-               street:   'Lamar',
-               shipping: {city:   'Georgetown',
-                          street: 'Austin'}
-    }
 
-    binder = Domkey::View::Binder.new payload: payload, view: AddressView.new
-    binder.set
-    extracted = binder.value
-    extracted.should eql payload
-  end
+  context 'View.binder convenience factory' do
 
-  it 'view view view' do
-    payload = {city:     'Austin',
-               street:   'Lamar',
-               shipping: {city:          'Georgetown',
-                          street:        'Austin',
-                          # this is view within a view within original view
-                          delivery_date: {month: 'delivery thing'}}
-    }
+    it 'single view' do
 
-    binder = Domkey::View::Binder.new payload: payload, view: AddressView.new
-    binder.set
-    extracted = binder.value
-    extracted.should eql payload
-  end
+      payload             = {city: 'Austin', fruit: ['tomato', 'other']}
+      binder              = AddressView.binder payload
 
-  it 'binder' do
-    payload = {city: 'Mordor'}
+      # default values when page loads before setting the values
+      default_page_values = {:city=>"id city class city", :fruit=>["other"]}
+      binder.value.should eql default_page_values
+      binder.set
 
-    view   = AddressView.new
-    binder = Domkey::View::Binder.new view: view, payload: payload
-    binder.set
-    scraped_payload = binder.value
+      extracted_payload = binder.value
+      extracted_payload.should eql payload
 
-    scraped_payload.should eql payload
-  end
-
-  it 'pageobject' do
-
-    payload             = {city: 'Austin', fruit: ['tomato', 'other']}
-    binder              = AddressView.binder payload
-
-    # default values when page loads before setting the values
-    default_page_values = {:city=>"id city class city", :fruit=>["other"]}
-    binder.value.should eql default_page_values
-    binder.set
-
-    extracted_payload = binder.value
-    extracted_payload.should eql payload
+    end
 
   end
 
