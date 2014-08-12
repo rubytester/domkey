d41fd152a102f55e95f659e96993f06a02da2bbe
rubytester <github@rubytester.com>
2014-08-12 16:44:13 -0500

    @key and @value for current pageobject being acted on

--- lib/domkey/view/binder.rb
+++ lib/domkey/view/binder.rb
@@ -50,11 +50,12 @@ module Domkey
       # set each pageobject in the view with the value from the payload
       def set
         @payload.each_pair do |key, value|
+          @key, @value = key, value
           b, a      = "before_#{key}".to_sym, "after_#{key}".to_sym
           bs, s, as = "before_set_#{key}".to_sym, "set_#{key}".to_sym, "after_set_#{key}".to_sym
           __send__(b) if respond_to?(b)
           __send__(bs) if respond_to?(bs)
-          respond_to?(s) ? __send__(s) : set_pageobject(key, value)
+          respond_to?(s) ? __send__(s) : set_pageobject
           __send__(as) if respond_to?(as)
           __send__(a) if respond_to?(a)
         end
@@ -66,11 +67,12 @@ module Domkey
       def value
         extracted = {}
         @payload.each_pair do |key, value|
+          @key, @value = key, value
           b, a      = "before_#{key}".to_sym, "after_#{key}".to_sym
           bv, v, av = "before_value_#{key}".to_sym, "value_#{key}".to_sym, "after_value_#{key}".to_sym
           __send__(b) if respond_to?(b)
           __send__(bv) if respond_to?(bv)
-          extracted[key] = respond_to?(v) ? __send__(v) : value_for_pageobject(key, value)
+          extracted[key] = respond_to?(v) ? __send__(v) : value_for_pageobject
           __send__(av) if respond_to?(av)
           __send__(a) if respond_to?(a)
         end
@@ -84,11 +86,12 @@ module Domkey
       def options
         extracted = {}
         @payload.each_pair do |key, value|
+          @key, @value = key, value
           b, a      = "before_#{key}".to_sym, "after_#{key}".to_sym
           bo, o, ao = "before_options_#{key}".to_sym, "options_#{key}".to_sym, "after_options_#{key}".to_sym
           __send__(b) if respond_to?(b)
           __send__(bo) if respond_to?(bo)
-          extracted[key] = respond_to?(o) ? __send__(o) : options_for_pageobject(key, value)
+          extracted[key] = respond_to?(o) ? __send__(o) : options_for_pageobject
           __send__(ao) if respond_to?(ao)
           __send__(a) if respond_to?(a)
         end
@@ -97,36 +100,36 @@ module Domkey
 
       private
 
-      def options_for_pageobject key, value
-        object = @view.send(key)
+      def options_for_pageobject
+        object = @view.send(@key)
         if object.method(:options).parameters.empty?
-          expected_to_be_present(object, key).options
+          expected_to_be_present(object).options
         else
-          expected_to_be_present(object, key).options value
+          expected_to_be_present(object).options @value
         end
       end
 
-      def set_pageobject key, value
-        expected_to_be_present(@view.send(key), key).set value
+      def set_pageobject
+        expected_to_be_present(@view.send(@key)).set @value
       end
 
-      def value_for_pageobject key, value
-        object = @view.send(key)
+      def value_for_pageobject
+        object = @view.send(@key)
         if object.method(:value).parameters.empty?
-          expected_to_be_present(object, key).value
+          expected_to_be_present(object).value
         else
-          expected_to_be_present(object, key).value value
+          expected_to_be_present(object).value @value
         end
       end
 
       # we expect pageobject to be present for interaction
       # if not present we raise NotFoundError.
       # Use Watir.default_timeout value to wait until element present
-      def expected_to_be_present(object, key)
+      def expected_to_be_present(object)
         object.wait_until_present if object.respond_to?(:wait_until_present)
         object
       rescue Watir::Wait::TimeoutError
-        raise Exception::NotFoundError, "Binder expected pageobject: '#{@view.class}##{key}' to be present"
+        raise Exception::NotFoundError, "Binder expected pageobject: '#{@view.class}##{@key}' to be present"
       end
     end
   end
