ea29eb06c74744e5ac4f092545c25f72db9f4adb
rubytester <github@rubytester.com>
2014-10-31 13:44:28 -0500

    since we don't peek at package these methods not needed

--- lib/domkey/view/widgetry/element_delegator.rb
+++ lib/domkey/view/widgetry/element_delegator.rb
@@ -5,17 +5,8 @@ module Domkey
       # PageObject delegate to Watir::Element
       module ElementDelegator
 
-        # warning: ActiveResource provides Object#present?
-        def present?
-          _element.present?
-        end
-
-        def wait_until_present(timeout = nil)
-          _element.wait_until_present
-        end
-
         # @api private
-        # delegate to element when element responds to message
+        # delegate to watir element if element responds to message
         def method_missing(message, *args, &block)
           if _element.respond_to?(message)
             _element.__send__(message, *args, &block)
@@ -25,14 +16,16 @@ module Domkey
         end
 
         # @api private
-        # ruturn true when element.respond_to? message so we can delegate with confidence
+        # delegate with confidence
         def respond_to_missing?(message, include_private = false)
           _element.respond_to?(message) || super
         end
 
         private
 
-        # extract first element strategy for pageobject with keys or just element
+        # for PageObject constructed with hash keys
+        # extract first element as representative element.
+        # XXX Sloppy strategy. Got better idea?
         def _element
           if element.kind_of?(Hash)
             element.first.last
