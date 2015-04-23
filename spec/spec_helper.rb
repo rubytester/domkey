fd60be79a623561817a16a4b7999a1af213a1047
rubytester <github@rubytester.com>
2015-04-23 17:00:31 -0500

    run tests in docker container

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,8 +25,18 @@ class LocalChromeBrowser < Domkey::Browser::Factory
   end
 end
 
+class DockerStandAloneDebug < Domkey::Browser::Factory
+
+  def factory
+    # Mac OS X with boot2docker
+    # docker run -d -p 5905:5900 -p 4444:4444 -v `pwd`:`pwd` -w `pwd` --name domkey_chrome rubytester/standalone-chrome-debug:41
+    # open vnc://:secret@$(boot2docker ip):5905
+    # http://$(boot2docker ip):4444/wd/hub
+    Watir::Browser.new :chrome, url: "http://192.168.59.103:4444/wd/hub"
+  end
 end
 
+#Domkey::Browser.factory = DockerStandAloneDebug.new
 Domkey::Browser.factory = LocalChromeBrowser.new
 
 RSpec.configure do |config|
